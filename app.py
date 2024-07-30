93939225013c795ef528579488107044d9b94002
Harish Vadaparty <harishvadaparty@gmail.com>
2024-07-30 09:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ api.add_resource(MSURLRerank, '/msurlrerank')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
